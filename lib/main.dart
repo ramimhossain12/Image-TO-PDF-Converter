d190bd64491647d97f204b98bb3a8397c876586e
ramimhossain12 <ramimhossain6242@gmail.com>
2021-03-15 23:49:53 +0600

    splash screen add

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,69 @@
-import 'dart:io';
-
-import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
+import 'package:lottie/lottie.dart';
 
-main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
+import 'menu.dart';
+
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Splash Screen',
+      theme: ThemeData(
+
+
+        primarySwatch: Colors.green,
+
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyAppState extends State<MyApp> {
-  final picker = ImagePicker();
-  final pdf = pw.Document();
-  List<File> _image = [];
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    //set time to load the new page
+    Future.delayed(Duration(seconds: 6), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Menu()));
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("image to pdf"),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.picture_as_pdf),
-              onPressed: () {
-                createPDF();
-                savePDF();
-              })
-        ],
+      backgroundColor: Colors.white,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                height: 300,
+                width: 300,
+                child: Lottie.asset('assets/splashback.json')),
+            SizedBox(height: 20),
+            Text(
+              "Dodo",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: getImageFromGallery,
-      ),
-      body: _image != null
-          ? ListView.builder(
-        itemCount: _image.length,
-        itemBuilder: (context, index) => Container(
-            height: 400,
-            width: double.infinity,
-            margin: EdgeInsets.all(8),
-            child: Image.file(
-              _image[index],
-              fit: BoxFit.cover,
-            )),
-      )
-          : Container(),
     );
-  }
-
-  getImageFromGallery() async {
-    final pickedFile = await picker.getImage(source: ImageSource.gallery);
-    setState(() {
-      if (pickedFile != null) {
-        _image.add(File(pickedFile.path));
-      } else {
-        print('No image selected');
-      }
-    });
-  }
-
-  createPDF() async {
-    for (var img in _image) {
-      final image = pw.MemoryImage(img.readAsBytesSync());
-
-      pdf.addPage(pw.Page(
-          pageFormat: PdfPageFormat.a4,
-          build: (pw.Context contex) {
-            return pw.Center(child: pw.Image(image));
-          }));
-    }
-  }
-
-  savePDF() async {
-    try {
-      final dir = await getExternalStorageDirectory();
-      final file = File('${dir.path}/filename.pdf');
-      await file.writeAsBytes(await pdf.save());
-      showPrintedMessage('success', 'saved to documents');
-    } catch (e) {
-      showPrintedMessage('error', e.toString());
-    }
-  }
-
-  showPrintedMessage(String title, String msg) {
-    Flushbar(
-      title: title,
-      message: msg,
-      duration: Duration(seconds: 3),
-      icon: Icon(
-        Icons.info,
-        color: Colors.blue,
-      ),
-    )..show(context);
   }
 }
---
+++ lib/menu.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+
+
+class Menu extends StatefulWidget {
+  Menu({Key key}) : super(key: key);
+
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  final picker = ImagePicker();
+  final pdf = pw.Document();
+  List<File> _image = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("Image To PDF"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.picture_as_pdf),
+              onPressed: () {
+                createPDF();
+                savePDF();
+              })
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_circle_up,),
+        onPressed: getImageFromGallery,
+      ),
+      body: _image != null
+          ? ListView.builder(
+        itemCount: _image.length,
+        itemBuilder: (context, index) => Container(
+            height: 400,
+            width: double.infinity,
+            margin: EdgeInsets.all(8),
+            child: Image.file(
+              _image[index],
+              fit: BoxFit.cover,
+            )),
+      )
+          : Container(),
+    );
+  }
+  getImageFromGallery() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image.add(File(pickedFile.path));
+      } else {
+        print('No image selected');
+      }
+    });
+  }
+  createPDF() async {
+    for (var img in _image) {
+      final image = pw.MemoryImage(img.readAsBytesSync());
+
+      pdf.addPage(pw.Page(
+          pageFormat: PdfPageFormat.a4,
+          build: (pw.Context contex) {
+            return pw.Center(child: pw.Image(image));
+          }));
+    }
+  }
+  savePDF() async {
+    try {
+      final dir = await getExternalStorageDirectory();
+      final file = File('${dir.path}/filename.pdf');
+      await file.writeAsBytes(await pdf.save());
+      showPrintedMessage('success', 'saved to documents');
+    } catch (e) {
+      showPrintedMessage('error', e.toString());
+    }
+  }
+  showPrintedMessage(String title, String msg) {
+    Flushbar(
+      title: title,
+      message: msg,
+      duration: Duration(seconds: 3),
+      icon: Icon(
+        Icons.info,
+        color: Colors.blue,
+      ),
+    )..show(context);
+  }
+}
